078c4736753b840950591568cb5a6fdf35af707a
Simon Persson <atlaspersson@outlook.com>
2025-04-26 12:48:34 +0200

    Added ProjectAssignment model

--- server/models/ProjectAssignment.js
+++ server/models/ProjectAssignment.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const projectAssignmentSchema = new mongoose.Schema({
-    employee: {},
-    project: {},
-    start_date: {}
+    employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+    project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
+    start_date: { type: Date, required: true }
 })
+
+module.exports = mongoose.model('ProjectAssignment', projectAssignmentSchema)
